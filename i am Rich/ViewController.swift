ad5945074c27156a1021f76ad29a80cf3c053c21
vineet srivastav <vineet9798@gmail.com>
2021-09-11 00:20:46 +0530

    Initial Commit

---
+++ i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by vineet srivastav on 11/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
